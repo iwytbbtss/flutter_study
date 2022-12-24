bf5e572bf14fe98f738a99daaca5c9cd44b499b6
Sun <iwytbbtss00@naver.com>
2022-12-24 18:23:43 +0900

    videos player init

---
+++ video_player/lib/screen/home_screen.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  PickedFile? video;
+
+  void _onLogoTap() async {
+    final selectedVideo =
+        await ImagePicker.platform.pickVideo(source: ImageSource.gallery);
+
+    if (selectedVideo != null) {
+      setState(() {
+        video = selectedVideo;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = TextStyle(
+        color: Colors.white, fontSize: 30, fontWeight: FontWeight.w300);
+
+    return Scaffold(
+      body: video == null
+          ? _BodyEmpty(
+              textStyle: textStyle,
+              onTap: _onLogoTap,
+            )
+          : _BodyVideo(),
+    );
+  }
+}
+
+class _BodyEmpty extends StatelessWidget {
+  const _BodyEmpty({
+    Key? key,
+    required this.textStyle,
+    required this.onTap,
+  }) : super(key: key);
+
+  final TextStyle textStyle;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Color(0xFF2A3A7C), Color(0xFF000118)])),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        GestureDetector(
+            onTap: onTap, child: Image.asset('assets/images/logo.png')),
+        const SizedBox(
+          height: 30,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'VIDEO',
+              style: textStyle,
+            ),
+            Text(
+              'PLAYER',
+              style: textStyle.copyWith(fontWeight: FontWeight.w700),
+            )
+          ],
+        )
+      ]),
+    );
+  }
+}
+
+class _BodyVideo extends StatelessWidget {
+  const _BodyVideo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('video'),
+    );
+  }
+}
